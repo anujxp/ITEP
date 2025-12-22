a5a6c625148778e36abd35750330a21a7f793b77
anujxp <ganuj293@gmail.com>
2025-12-22 23:07:05 +0530

    updated first REst Api

--- spring-boot/bootweb/src/main/java/com/info/bootweb/controller/ProductController.java
+++ spring-boot/bootweb/src/main/java/com/info/bootweb/controller/ProductController.java
@@ -1,11 +1,55 @@
 package com.info.bootweb.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-@Controller
-@RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.info.bootweb.BootwebApplication;
+import com.info.bootweb.enity.Product;
+import com.info.bootweb.service.ProductService;
 
+import java.util.List;
+import java.util.Scanner;
+
+@RestController
+@RequestMapping("/product")
 public class ProductController {
 
+    private final BootwebApplication bootwebApplication;
+
+private final ProductService  productService;
+@Autowired
+public ProductController(ProductService productsService, BootwebApplication bootwebApplication){
+    this.productService = productsService;
+    this.bootwebApplication = bootwebApplication;
+}
+	
+	
+
+	  @PostMapping
+	  
+	  public Product save(@RequestBody Product p) {
+		  return productService.saveProduct(p);
+	  }
+	  
+	  @GetMapping
+	  
+	  public List<Product> getList(){
+		  return productService.getList();
+	  }
+	  @DeleteMapping("/{id}")
+	  public List<Product> delete(@PathVariable int id ) {
+			  productService.delete(id);
+			 return productService.getList();
+			 		 		  
+	  }
+	  
 }
--- spring-boot/bootweb/src/main/java/com/info/bootweb/service/ProductService.java
+++ spring-boot/bootweb/src/main/java/com/info/bootweb/service/ProductService.java
@@ -1,4 +1,32 @@
 package com.info.bootweb.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.info.bootweb.enity.Product;
+import com.info.bootweb.repo.Productepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
-}
+
+	private final Productepository productRepo;
+
+	@Autowired
+	public ProductService(Productepository productRepo) {
+		this.productRepo = productRepo;
+	}
+
+	public Product saveProduct(Product p) {
+		return this.productRepo.save(p);
+	}
+
+	public List<Product> getList() {
+		return productRepo.findAll();
+	}
+	public void delete(int id ) {
+	productRepo.deleteById(id);
+	}
+	
+	}
