ff58cc9758afad71ee721e9acf09e7a6cee4d57b
anujxp <ganuj293@gmail.com>
2026-02-02 22:10:57 +0530

    first react practice application

---
+++ React/Practice-Projects/Student-App/src/Data.js
@@ -0,0 +1,33 @@
+export default[
+    {
+        "roll" : 1001,
+        "name" : "anuj",
+        "gender" : "male",
+        "branch" : "CS"
+    },
+    {
+        "roll" : 1002,
+        "name" : "Atul",
+        "gender" : "male",
+        "branch" : "IT"
+    },
+    {
+        "roll" : 1003,
+        "name" : "Sunny",
+        "gender" : "male",
+        "branch" : "CV"
+    },
+    {
+        "roll" : 1004,
+        "name" : "Ankita",
+        "gender" : "female",
+        "branch" : "CS"
+    },
+    {
+        "roll" : 1005,
+        "name" : "Swara",
+        "gender" : "female",
+        "branch" : "Mech"
+
+    }
+];
