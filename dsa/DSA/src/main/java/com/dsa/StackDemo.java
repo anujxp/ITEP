81c49644473860eabd2cd45861a17f8f9902c01b
anujxp <ganuj293@gmail.com>
2026-02-20 01:17:23 +0530

    stack update

--- dsa/DSA/src/main/java/com/dsa/StackDemo.java
+++ dsa/DSA/src/main/java/com/dsa/StackDemo.java
@@ -1,4 +1,38 @@
 package com.dsa;
 
+class Stack{
+    private int[]arr = new int[5];
+    private int top;
+    private int size;
+public Stack(){
+    size = arr.length;
+    top = -1;
+}
+
+    public void push(int data) {
+    if(top<size)
+    arr[++top] = data;
+    else
+        System.out.println("Stack overflow");
+    }
+    public int pop(){
+    return arr[top--];
+    }
+    public int peek() {
+    return arr[top];
+    }
+}
+
+
 public class StackDemo {
+    public static void main(String[] args){
+        Stack nums = new Stack();
+        nums.push(1);
+        nums.push(2);
+        nums.push(3);
+        System.out.println(nums.peek());
+        nums.pop();
+        System.out.println(nums.peek());
+//        System.out.println(nums.sear
+    }
 }
