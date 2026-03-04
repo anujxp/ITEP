0ec04911836881d9a5fcb962b402cc3ba7f05830
anujxp <ganuj293@gmail.com>
2026-03-04 22:52:51 +0530

    React Recux Demo

---
+++ React/Practice-Projects/Redux-Demo/src/Redux-configuration/CounterSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const slice = createSlice({
+    name : "Counter-slice",
+    initialState:{
+        counter : 100
+    },
+    reducers: {
+         incrementCounter: (state,action)=>{
+            state.counter = state.counter + 1
+        },
+        decrementCounter: (state,action)=>{
+            state.counter = state.counter - 1;
+        }
+    }
+});
+
+
+export const {incrementCounter,decrementCounter} = slice.actions;
+export default slice.reducer;
