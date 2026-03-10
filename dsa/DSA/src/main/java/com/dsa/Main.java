eb257766cf244828678bb2a057b4856b3e912a6e
anujxp <ganuj293@gmail.com>
2026-03-10 11:25:41 +0530

    frequently asked question

--- dsa/DSA/src/main/java/com/dsa/Main.java
+++ dsa/DSA/src/main/java/com/dsa/Main.java
@@ -5,13 +5,13 @@ import org.jetbrains.annotations.NotNull;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void selectionSort(int arr[]){
+    public static void selectionSort(int arr[]) {
         int n = arr.length;
         int minIndex;
-        for (int i = 0; i <n; i++) {
+        for (int i = 0; i < n; i++) {
             minIndex = i;
-            for (int j = i+1; j <n ; j++) {
-                if(arr[minIndex] > arr[j])
+            for (int j = i + 1; j < n; j++) {
+                if (arr[minIndex] > arr[j])
                     minIndex = j;
             }
             int temp = arr[minIndex];
@@ -20,33 +20,38 @@ public class Main {
             System.out.println(arr[i]);
         }
     }
-    public static void insertionSort( int[] arr){
+
+
+    public Main() {
+    }
+
+    public static void insertionSort(int[] arr) {
         int n = arr.length;
-        int i,j,key;
-        for ( i = 1; i < n; i++) {
+        int i, j, key;
+        for (i = 1; i < n; i++) {
             key = arr[i];
-            j=i-1;
-            while(key < arr[j] && j>=0) {
+            j = i - 1;
+            while (key < arr[j] && j >= 0) {
                 arr[j + 1] = arr[j];
                 j--;
             }
-            arr[j+1] = key;
+            arr[j + 1] = key;
         }
 
-        
+
     }
+
     public static void main(String[] args) {
-        int arr[] = {1,4,2,5,9,6,7};
+        int arr[] = {1, 4, 2, 5, 9, 6, 7};
         System.out.println("before sorting");
-        
-        for(int s : arr) System.out.print(s+ " ");
-        
+
+        for (int s : arr) System.out.print(s + " ");
+
         selectionSort(arr);
         System.out.println("\nafter sorting");
-        
-        for(int s : arr) System.out.print(s+ " ");
-        
+
+        for (int s : arr) System.out.print(s + " ");
 
 
-   }
+    }
 }
