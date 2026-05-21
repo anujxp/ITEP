bd0805f1674eb372341d32c1480b44fcf4e19251
anujxp <ganuj293@gmail.com>
2026-05-21 11:12:32 +0530

    dsa question udpate

--- dsa/DSA/src/main/java/com/dsa/LinkedListDemo.java
+++ dsa/DSA/src/main/java/com/dsa/LinkedListDemo.java
@@ -6,11 +6,13 @@ class Node {
 
     public Node(int data) {
         this.data = data;
+        this.next = null;
     }
 }
 
 class LinkedList {
     Node head = null;
+
 
     public void add(int data) {
         Node newNode = new Node(data);
@@ -24,7 +26,81 @@ class LinkedList {
         }
     }
 
-    public void printValues() {
+    public void insertAtBeg(int data){
+        Node temp = new Node(data);
+        temp.next = head;
+        head = temp;
+    }
+    public void insertEnd(int data){
+        Node temp = new Node(data);
+        if(head == null) head = temp;
+        else{
+            Node last = head;
+            while(last.next != null){
+                last = last.next;
+            }
+            last.next = temp;
+        }
+        System.out.println("Nde inserted at the End....");
+    }
+
+    public void insertPos(int data , int pos){
+        Node temp = new Node(data), last;
+        last = head;
+        int count = 0;
+        while (last != null){
+            count++;
+            last = last.next;
+        }
+        if(pos <= 0 || pos>count+1){
+            System.out.println("Position is invalid");
+            return;
+        }
+        if(pos == 1) {
+            temp.next = head;
+            head = temp;
+        }
+        else{
+            last = head;
+            for(int i = 2;i< pos; i++){
+                last = last.next;
+            }
+            temp.next = last.next;
+            last.next = temp;
+
+        }
+      }
+
+    public void deleteFirst(){
+        if(head == null)
+        {
+
+            System.out.println("Lisked list is Empty");
+            return;
+        }
+        head = head.next;
+    }
+
+    public void deleteLast(){
+        if(head == null)
+        {
+            System.out.println("List is Empty");
+            return;
+        }
+        if(head.next == null){
+            head = null;
+            return;
+        }
+        Node p = head;
+        while(p.next.next != null){
+            p =p.next;
+        }
+        p.next = null;
+
+    }
+    
+
+    public void display() {
         Node current = head;
         while (current != null) {
             System.out.print(current.data + " ");
@@ -33,7 +109,12 @@ class LinkedList {
         System.out.println();
     }
 
-    public void delete(int data){
+    public void deleteByValue(int data){
+        if(head == null) return;
+        if(head.data == data){
+            head= head.next;
+            return;
+        }
         Node current = head;
         while(current.next!= null && current.next.data != data){
             current = current.next;
@@ -51,9 +132,10 @@ public class LinkedListDemo {
         nums.add(5);
         nums.add(6);
         nums.add(3);
-        nums.printValues();
-        nums.delete(6);
-        nums.printValues();
+        
+        nums.display();
+        nums.deleteByValue(6);
+        nums.display();
 
     }
 }
